bd70a7ae8cc6f284a300869f9bc01beac90af24c
olegkhmil <dactiloscopist@mail.com>
2020-04-27 10:51:01 +0300

    AbstractDao realisation

--- src/main/java/com/javamentor/qa/platform/dao/abstracrt/model/AbstractDAO.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracrt/model/AbstractDAO.java
@@ -1,5 +1,22 @@
 package com.javamentor.qa.platform.dao.abstracrt.model;
 
-public interface AbstractDAO {
+import java.util.List;
 
+public interface AbstractDAO<T, PK> {
+
+    void persist(T t);
+
+    void update(T t);
+
+    void delete (T t);
+
+    void deleteByKeyCascadeEnable(PK id);
+
+    void deleteByKeyCascadeIgnore(PK id);
+
+    boolean existsById(PK id);
+
+    T getByKey(PK id);
+
+    List<T> getAll();
 }
