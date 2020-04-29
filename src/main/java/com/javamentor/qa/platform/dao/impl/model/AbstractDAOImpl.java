d3b2fd05ab051c5fccc8899d794d767ec492bdfa
olegkhmil <dactiloscopist@mail.com>
2020-04-29 21:20:49 +0300

    AbstractDao realisation

---
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/AbstractDAOImpl.java
@@ -0,0 +1,71 @@
+package com.javamentor.qa.platform.dao.impl.model;
+
+import com.javamentor.qa.platform.dao.abstracrt.model.AbstractDAO;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+@Repository
+public class AbstractDAOImpl<T, PK> implements AbstractDAO<T, PK> {
+
+    private Class<T> clazz;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void setClazz(Class<T> clazzToSet) {
+        this.clazz = clazzToSet;
+    }
+
+    @Override
+    public void persist(T t) {
+        entityManager.persist(t);
+    }
+
+    @Override
+    public void update(T t) {
+        entityManager.merge(t);
+    }
+
+    @Override
+    public void delete(T t) {
+        entityManager.remove(t);
+    }
+
+    @Override
+    public void deleteByKeyCascadeEnable(PK id) {
+        if(existsById(id)){
+            entityManager.remove(getByKey(id));
+        }
+    }
+
+    @Override
+    public void deleteByKeyCascadeIgnore(PK id) {
+        if(existsById(id)){
+            Query query = entityManager.createQuery(
+                    "DELETE FROM " +clazz.getName() + " u WHERE u.id = :id" );
+            query.setParameter("id", id);
+            int rowsDeleted = query.executeUpdate();
+            System.out.println("entities deleted: " + rowsDeleted);
+        }
+    }
+
+    @Override
+    public boolean existsById(PK id) {          //id or Key
+        return entityManager.find(clazz, id) != null;
+    }
+
+    @Override
+    public T getByKey(PK id) {
+        return entityManager.find(clazz, id);
+    }
+
+    @Override
+    public List<T> getAll() {
+        return entityManager.createQuery("from " + clazz.getName()).getResultList();
+    }
+}
