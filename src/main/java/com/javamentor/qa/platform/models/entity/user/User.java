538f343ecea0902c1e540e3a06abf0130d463a0f
AAgalakov <andrew_agalakov@mail.ru>
2020-05-03 17:19:26 +0300

    Add Builder

--- src/main/java/com/javamentor/qa/platform/models/entity/user/User.java
+++ src/main/java/com/javamentor/qa/platform/models/entity/user/User.java
@@ -1,9 +1,6 @@
 package com.javamentor.qa.platform.models.entity.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -23,6 +20,7 @@ import java.util.Objects;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name = "users")
 public class User implements UserDetails {
 
--- src/main/java/com/javamentor/qa/platform/webapp/configs/Application.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/Application.java
@@ -1,9 +1,13 @@
 package com.javamentor.qa.platform.webapp.configs;
 
+import com.javamentor.qa.platform.webapp.configs.initializer.TestEntityInit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @ComponentScan("com.javamentor.qa.platform")
@@ -13,6 +17,12 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean(initMethod = "init")
+    @PostConstruct
+    public TestEntityInit initTestData() {
+        return new TestEntityInit();
     }
 
 }
