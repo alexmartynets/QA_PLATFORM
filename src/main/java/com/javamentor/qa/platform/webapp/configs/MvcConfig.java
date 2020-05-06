f04b7515d5983f2a3eea75a95cc177f4d50ca942
igor <predator.1199@mail.ru>
2020-05-06 11:13:47 +0700

    Добавил конфиг для обычных контроллеров а так же пакеты для фронта

---
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/MvcConfig.java
@@ -0,0 +1,27 @@
+package com.javamentor.qa.platform.webapp.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/"};
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+
+    }
+}
