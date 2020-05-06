be7636455c8720d650da448407d62cd790b98a26
Alexandr Martynets <amartinets@yandex.ru>
2020-05-06 21:43:35 +0300

    Temporary project for internal use! Don't Merge!

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/QuestionDaoDtoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/QuestionDaoDtoImpl.java
@@ -3,19 +3,13 @@ package com.javamentor.qa.platform.dao.impl.dto;
 import com.javamentor.qa.platform.dao.abstracrt.dto.QuestionDaoDto;
 import com.javamentor.qa.platform.dao.impl.model.AbstractDAOImpl;
 import com.javamentor.qa.platform.models.dto.QuestionDto;
-import com.javamentor.qa.platform.models.dto.TagDto;
 import org.hibernate.query.Query;
 import org.hibernate.transform.ResultTransformer;
-import org.hibernate.transform.ToListResultTransformer;
-import org.hibernate.transform.Transformers;
 import org.springframework.stereotype.Repository;
 
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Repository
 public class QuestionDaoDtoImpl extends AbstractDAOImpl<QuestionDto, Long> implements QuestionDaoDto {
@@ -26,13 +20,13 @@ public class QuestionDaoDtoImpl extends AbstractDAOImpl<QuestionDto, Long> imple
         List<QuestionDto> questionDto = new ArrayList<>();
         try {
             questionDto = entityManager.createQuery("SELECT " +
-                    "q.id AS id, " +
-                    "q.title AS title, " +
-                    "q.user.fullName AS username, " +
-                    "q.user.reputationCount AS reputationCount, " +
-                    "q.viewCount AS viewCount, " +
-                    "q.countValuable AS countValuable, " +
-                    "q.persistDateTime AS persistDateTime " +
+                    "q.id, " +
+                    "q.title, " +
+                    "q.user.fullName, " +
+                    "q.user.reputationCount, " +
+                    "q.viewCount, " +
+                    "q.countValuable, " +
+                    "q.persistDateTime " +
                     "FROM Question q")
                     .unwrap(Query.class)
                     .setResultTransformer(new ResultTransformer() {
@@ -60,34 +54,4 @@ public class QuestionDaoDtoImpl extends AbstractDAOImpl<QuestionDto, Long> imple
         }
         return questionDto;
     }
-
-//    private List<TagDto> getTagsByQuestionId(@NotNull Long questionId) {
-//        List<TagDto> tags = new ArrayList<>();
-//        try {
-//           tags = entityManager.createNativeQuery("SELECT t.name FROM tag t LEFT JOIN question_has_tag qht on t.id = qht.tag_id WHERE qht.question_id = :questionId")
-//                   .setParameter("questionId", questionId).getResultList();
-//       } catch (Exception e){
-//           e.printStackTrace();
-//       }
-//        return tags;
-//    }
-//
-//    public List<QuestionDto> getListQuestionDtoByListQuestionId(List<Long> listQuestionId) {
-//        return listQuestionId
-//                .stream()
-//                .map(this::getQuestionDtoByQuestionId)
-//                .filter(Optional::isPresent)
-//                .map(Optional::get)
-//                .collect(Collectors.toList());
-//    }
-//
-//    public List<Long> getListQuestionId() {
-//        List<Long> questionIdList = new ArrayList<>();
-//        try {
-//            questionIdList = entityManager.createNativeQuery("SELECT question.id FROM question").getResultList();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return questionIdList;
-//    }
 }
