8fc3323f0a43bc7aa15ddc84835e76f66be340e7
Alexandr Martynets <amartinets@yandex.ru>
2020-05-06 12:43:37 +0300

    Temporary project for internal use! Don't Merge!

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionResourceController.java
@@ -1,10 +1,8 @@
 package com.javamentor.qa.platform.webapp.controllers;
 
-import com.javamentor.qa.platform.dao.abstracrt.dto.QuestionDaoDto;
 import com.javamentor.qa.platform.models.dto.QuestionDto;
 import com.javamentor.qa.platform.service.abstracrt.QuestionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
