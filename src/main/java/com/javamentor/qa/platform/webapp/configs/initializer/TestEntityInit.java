56a2fde0cfe82a420a59955224c2f33d6121da44
AAgalakov <andrew_agalakov@mail.ru>
2020-05-07 11:13:30 +0300

    Refactoring. With out builder TestEntityInit

--- src/main/java/com/javamentor/qa/platform/webapp/configs/initializer/TestEntityInit.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/initializer/TestEntityInit.java
@@ -2,9 +2,7 @@ package com.javamentor.qa.platform.webapp.configs.initializer;
 
 import com.javamentor.qa.platform.service.impl.TestDataEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
 public class TestEntityInit {
 
     @Autowired
