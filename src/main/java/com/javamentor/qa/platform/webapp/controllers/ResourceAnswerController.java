5bd0faa355befc02c3cc95d1cc785ee741a262b6
AAgalakov <andrew_agalakov@mail.ru>
2020-05-09 12:48:01 +0300

    Refactoring

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/ResourceAnswerController.java
+++
@@ -1,9 +0,0 @@
-package com.javamentor.qa.platform.webapp.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("api/user/question/{questionId}/answer")
-public class ResourceAnswerController {
-}
