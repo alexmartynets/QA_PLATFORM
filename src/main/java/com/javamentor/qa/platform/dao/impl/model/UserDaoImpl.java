66c7ba739b6df16b6934578f5a96d38ed371ec76
Alexandr Martynets <amartinets@yandex.ru>
2020-05-09 09:40:26 +0300

    Temporary project for internal use! Don't Merge!

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDaoImpl.java
@@ -1,6 +1,7 @@
 package com.javamentor.qa.platform.dao.impl.model;
 
 import com.javamentor.qa.platform.dao.abstracts.model.UserDao;
+import com.javamentor.qa.platform.models.entity.user.User;
 
-public class UserDaoImpl extends ReadWriteDaoImpl<T, PK> implements UserDao {
+public class UserDaoImpl extends ReadWriteDaoImpl<User, Long> implements UserDao {
 }
