68e83a713a83ef8c7aaf00d76af7388bbc5bd259
Alexandr Martynets <amartinets@yandex.ru>
2020-05-09 09:36:07 +0300

    Temporary project for internal use! Don't Merge!

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/UserService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/UserService.java
@@ -1,7 +1,4 @@
 package com.javamentor.qa.platform.service.abstracts.model;
 
-import com.javamentor.qa.platform.dao.abstracts.model.UserDao;
-
-
-public interface UserService<T, PK> extends UserDao<T, PK> {
+public interface UserService<T, PK> extends ReadWriteService<T, PK> {
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/model/UserServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/UserServiceImpl.java
@@ -1,9 +1,10 @@
 package com.javamentor.qa.platform.service.impl.model;
 
-import com.javamentor.qa.platform.dao.impl.model.ReadWriteDaoImpl;
 import com.javamentor.qa.platform.models.entity.user.User;
+import com.javamentor.qa.platform.service.abstracts.model.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl extends ReadWriteDaoImpl<User, Long> {
+public class UserServiceImpl extends ReadWriteServiceImpl<User, Long> implements UserService {
+
 }
