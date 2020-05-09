5df35cbe4d5d5ea781636f0adb2883fb6b74ae20
Alexandr Martynets <amartinets@yandex.ru>
2020-05-09 09:30:02 +0300

    Temporary project for internal use! Don't Merge!

--- src/main/java/com/javamentor/qa/platform/service/impl/model/ReadWriteServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/ReadWriteServiceImpl.java
@@ -1,47 +1,61 @@
 package com.javamentor.qa.platform.service.impl.model;
 
+import com.javamentor.qa.platform.dao.abstracts.model.ReadWriteDao;
 import com.javamentor.qa.platform.service.abstracts.model.ReadWriteService;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public class ReadWriteServiceImpl<T, PK> implements ReadWriteService<T, PK> {
+
+    private final ReadWriteDao<T, PK> readWriteDao;
+
+    protected ReadWriteServiceImpl(ReadWriteDao<T, PK> readWriteDao) {
+        this.readWriteDao = readWriteDao;
+    }
+
     @Override
+    @Transactional
     public void persist(T t) {
-
+        readWriteDao.persist(t);
     }
 
     @Override
+    @Transactional
     public void update(T t) {
-
+        readWriteDao.update(t);
     }
 
     @Override
+    @Transactional
     public void delete(T t) {
-
+        readWriteDao.delete(t);
     }
 
     @Override
+    @Transactional
     public void deleteByKeyCascadeEnable(PK id) {
-
+        readWriteDao.deleteByKeyCascadeEnable(id);
     }
 
     @Override
+    @Transactional
     public void deleteByKeyCascadeIgnore(PK id) {
-
+        readWriteDao.deleteByKeyCascadeIgnore(id);
     }
 
     @Override
     public boolean existsById(PK id) {
-        return false;
+        return readWriteDao.existsById(id);
     }
 
     @Override
     public T getByKey(PK id) {
-        return null;
+        return readWriteDao.getByKey(id);
     }
 
     @Override
     public List<T> getAll() {
-        return null;
+        return readWriteDao.getAll();
     }
 }
