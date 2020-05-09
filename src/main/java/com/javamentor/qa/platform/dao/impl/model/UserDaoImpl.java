f01a3d5d97a6041c1ba0b0ba868e3a39502bf986
Alexandr Martynets <amartinets@yandex.ru>
2020-05-09 09:46:24 +0300

    Temporary project for internal use! Don't Merge!

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDaoImpl.java
@@ -2,6 +2,8 @@ package com.javamentor.qa.platform.dao.impl.model;
 
 import com.javamentor.qa.platform.dao.abstracts.model.UserDao;
 import com.javamentor.qa.platform.models.entity.user.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UserDaoImpl extends ReadWriteDaoImpl<User, Long> implements UserDao {
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/model/UserServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.javamentor.qa.platform.service.impl.model;
 
 import com.javamentor.qa.platform.dao.abstracts.model.ReadWriteDao;
+import com.javamentor.qa.platform.dao.abstracts.model.UserDao;
 import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.model.UserService;
 import org.springframework.stereotype.Service;
@@ -8,7 +9,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ReadWriteServiceImpl<User, Long> implements UserService {
 
-    protected UserServiceImpl(ReadWriteDao<User, Long> readWriteDao) {
+    private final UserDao userDao;
+
+    protected UserServiceImpl(ReadWriteDao<User, Long> readWriteDao, UserDao userDao) {
         super(readWriteDao);
+        this.userDao = userDao;
     }
 }
