2d3a8132707caae1899f3aadc33fc8de43c3dd1e
olegkhmil <dactiloscopist@mail.com>
2020-05-10 17:12:12 +0300

    answer api

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/model/AnswerDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/model/AnswerDao.java
@@ -2,5 +2,8 @@ package com.javamentor.qa.platform.dao.abstracts.model;
 
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 
+import java.util.List;
+
 public interface AnswerDao extends ReadWriteDao<Answer, Long> {
+    List<Answer> getAnswersByQuestionID(Long questionId);
 }
