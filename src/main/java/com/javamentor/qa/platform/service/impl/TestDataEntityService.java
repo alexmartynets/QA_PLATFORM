6769d85db1e4a3e60cae57eafa32ff05760732dd
AAgalakov <andrew_agalakov@mail.ru>
2020-05-11 12:03:05 +0300

    Refactor TestDataEntityService

--- src/main/java/com/javamentor/qa/platform/service/impl/TestDataEntityService.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/TestDataEntityService.java
@@ -16,12 +16,14 @@ import com.javamentor.qa.platform.service.abstracts.model.comment.CommentAnswerS
 import com.javamentor.qa.platform.service.abstracts.model.comment.CommentQuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Transactional
 public class TestDataEntityService {
 
     @Autowired
