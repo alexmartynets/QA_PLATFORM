f6836b931fe8f3ab814049207ff49ef582fcb0f6
Alexandr Martynets <amartinets@yandex.ru>
2020-05-12 16:54:54 +0300

    final

--- src/main/java/com/javamentor/qa/platform/dao/util/SingleResultUtil.java
+++ src/main/java/com/javamentor/qa/platform/dao/util/SingleResultUtil.java
@@ -1,5 +1,6 @@
 package com.javamentor.qa.platform.dao.util;
 
+import org.hibernate.NonUniqueResultException;
 import org.hibernate.query.Query;
 
 import javax.persistence.NoResultException;
@@ -8,17 +9,12 @@ import java.util.Optional;
 public class SingleResultUtil {
 
     public static <T> Optional<T> getSingleResultOrNull(Query<T> var) {
-
         T result;
-
         try {
-
             result = var.getSingleResult();
         } catch (NoResultException e) {
-
             return Optional.empty();
         }
-
         return Optional.of(result);
     }
 }
