66d3443dddcbaaf49ffb552ffbd77ad1f9188e59
Alexandr Martynets <amartinets@yandex.ru>
2020-05-12 17:40:13 +0300

    final

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/UserResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/UserResourceController.java
@@ -2,13 +2,17 @@ package com.javamentor.qa.platform.webapp.controllers;
 
 import com.javamentor.qa.platform.models.dto.UserDto;
 import com.javamentor.qa.platform.models.entity.user.User;
+import com.javamentor.qa.platform.models.util.action.OnCreate;
+import com.javamentor.qa.platform.models.util.action.OnUpdate;
 import com.javamentor.qa.platform.service.abstracts.dto.UserDtoService;
 import com.javamentor.qa.platform.service.abstracts.model.UserService;
 import com.javamentor.qa.platform.webapp.converter.UserConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,7 +30,8 @@ public class UserResourceController {
     private UserConverter userConverter;
 
     @PostMapping
-    public ResponseEntity<UserDto> addUser(@RequestBody UserDto userDto) {
+    @Validated(OnCreate.class)
+    public ResponseEntity<UserDto> addUser(@RequestBody @Valid UserDto userDto) {
         userService.persist(userConverter.toEntity(userDto));
         return ResponseEntity.ok().body(userDto);
     }
@@ -37,7 +42,8 @@ public class UserResourceController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody UserDto userDto) {
+    @Validated(OnUpdate.class)
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody @Valid UserDto userDto) {
         User user = userConverter.toEntity(userDto);
         user.setId(id);
         userService.update(user);
