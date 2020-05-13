3602bf82115f59e10b61a9a1d309764afd7d79b6
AAgalakov <andrew_agalakov@mail.ru>
2020-05-13 20:40:41 +0300

    Добавил метод getQuestionById

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/QuestionDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/QuestionDtoDaoImpl.java
@@ -78,9 +78,68 @@ public class QuestionDtoDaoImpl extends ReadWriteDAOImpl<QuestionDto, Long> impl
         }
         return questionDto;
     }
-
+    //todo настроить
     @Override
     public QuestionDto getQuestionDtoById(Long id) {
-        return null;
+        QuestionDto questionDto = null;
+        try {
+            questionDto = (QuestionDto) entityManager.createQuery("SELECT " +
+                    "q.id, " +
+                    "q.title, " +
+                    "q.user.fullName, " +
+                    "q.user.reputationCount, " +
+                    "q.viewCount, " +
+                    "q.countValuable, " +
+                    "q.persistDateTime,  " +
+                    "t.id, " +
+                    "t.name, " +
+                    "t.description, " +
+                    "(SELECT COUNT (a) FROM Answer a WHERE a.question.id = q.id), " +
+                    "(SELECT a.isHelpful FROM Answer a WHERE a.question.id = q.id) " +
+                    "FROM Question q JOIN q.tags t")
+                    .unwrap(Query.class)
+                    .setResultTransformer(new ResultTransformer() {
+                        @Override
+                        public Object transformTuple(Object[] objects, String[] strings) {
+                            TagDto tagDto = TagDto.builder()
+                                    .id((Long) objects[7])
+                                    .name((String) objects[8])
+                                    .description((String) objects[9])
+                                    .build();
+                            List<TagDto> tagDtoList = new ArrayList<>();
+                            tagDtoList.add(tagDto);
+                            return QuestionDto.builder()
+                                    .id((Long) objects[0])
+                                    .title((String) objects[1])
+                                    .username((String) objects[2])
+                                    .reputationCount((Integer) objects[3])
+                                    .viewCount((Integer) objects[4])
+                                    .countValuable((Integer) objects[5])
+                                    .persistDateTime((LocalDateTime) objects[6])
+                                    .tags(tagDtoList)
+                                    .countAnswer(((Number) objects[10]).intValue())
+                                    .isHelpful((Boolean) objects[11])
+                                    .build();
+                        }
+
+                        @Override
+                        public List transformList(List list) {
+                            Map<Long, QuestionDto> result = new TreeMap<>(Comparator.reverseOrder());
+                            for (Object obj : list) {
+                                QuestionDto questionDto = (QuestionDto) obj;
+                                if (result.containsKey(questionDto.getId())) {
+                                    result.get(questionDto.getId()).getTags().addAll(questionDto.getTags());
+                                } else {
+                                    result.put(questionDto.getId(), questionDto);
+                                }
+                            }
+                            return new ArrayList<>(result.values());
+                        }
+                    })
+                    .getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return questionDto;
     }
 }
