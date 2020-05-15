7dece3c129020527b4153e294d86d9ff85fa4d1d
Artem <torbeev.art@yandex.ru>
2020-05-15 10:44:58 +0300

    pagination users

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/QuestionResourceController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/user/question")
 public class QuestionResourceController {
 
     @GetMapping
