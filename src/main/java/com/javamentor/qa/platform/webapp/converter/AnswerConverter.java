21c5017f060ad6bccae09a3cdf3acec9fb44b011
oleg <dactiloscopist@mail.ru>
2020-05-18 19:19:33 +0300

    to merge

--- src/main/java/com/javamentor/qa/platform/webapp/converter/AnswerConverter.java
+++ src/main/java/com/javamentor/qa/platform/webapp/converter/AnswerConverter.java
@@ -7,6 +7,7 @@ import com.javamentor.qa.platform.models.entity.user.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.Named;
 
 import javax.sql.rowset.serial.SerialBlob;
 
@@ -31,18 +32,19 @@ public abstract class AnswerConverter {
     public abstract Answer dtoToAnswer(AnswerDto answerDto);
 
     @Mappings({
-            @Mapping(target = "imageUser", expression = "java(toBlob(userDto.getImageUser()))"),
+            @Mapping(target = "imageUser", source = "imageUser", qualifiedByName = "toBlob"),
             @Mapping(target = "role.name", source = "role")
     })
     public abstract User dtoToUser(UserDto userDto);
 
     @Mappings({
-            @Mapping(target = "imageUser", expression = "java(toByte(user.getImageUser()))"),
+            @Mapping(target = "imageUser", source = "imageUser", qualifiedByName = "toByte"),
             @Mapping(target = "role", source = "role.name")
     })
     public abstract UserDto userToDto(User user);
 
-    public static byte[] toByte(Blob blob) {
+    @Named("toByte")
+    protected byte[] toByte(Blob blob) {
         try {
             if (blob != null)
                 return blob.getBytes(1, (int) blob.length());
@@ -52,7 +54,8 @@ public abstract class AnswerConverter {
         return null;
     }
 
-    public static Blob toBlob(byte[] bytes) {
+    @Named("toBlob")
+    protected Blob toBlob(byte[] bytes) {
         try {
             if (bytes != null)
                 return new SerialBlob(bytes);
