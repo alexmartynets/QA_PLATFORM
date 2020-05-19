129c7435a9e2fa33dd1939373ae62fbc67b687a4
Alexandr Martynets <amartinets@yandex.ru>
2020-05-19 15:11:16 +0300

    final

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/RoleDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/RoleDaoImpl.java
@@ -1,9 +1,15 @@
 package com.javamentor.qa.platform.dao.impl.model;
 
-import com.javamentor.qa.platform.dao.abstracts.model.RoleDao;
+import com.javamentor.qa.platform.dao.abstracts.model.RoleDAO;
 import com.javamentor.qa.platform.models.entity.user.Role;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public class RoleDaoImpl extends ReadWriteDAOImpl<Role, Long> implements RoleDao {
+public class RoleDaoImpl extends ReadWriteDAOImpl<Role, Long> implements RoleDAO {
+    @Override
+    public Optional<Role> getByRoleName(String roleName) {
+        return Optional.empty();
+    }
 }
