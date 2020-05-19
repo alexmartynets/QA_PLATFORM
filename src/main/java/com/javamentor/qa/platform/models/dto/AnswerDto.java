834c975791aa384400d6ea921bd1e0ff03f6d5c9
oleg <dactiloscopist@mail.ru>
2020-05-19 19:52:27 +0300

    answer_api

--- src/main/java/com/javamentor/qa/platform/models/dto/AnswerDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/AnswerDto.java
@@ -1,8 +1,11 @@
 package com.javamentor.qa.platform.models.dto;
 
+import com.javamentor.qa.platform.models.util.action.OnCreate;
+import com.javamentor.qa.platform.models.util.action.OnUpdate;
 import lombok.*;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import java.sql.Blob;
 import java.time.LocalDateTime;
 
@@ -13,14 +16,23 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class AnswerDto {
+
+    @Null(groups = OnCreate.class, message = "Должно принимать null значение при создании")
     private Long id;
-    @NotNull
+
+    @NotNull(groups = OnUpdate.class, message = "Не должно принимать null значение при обновлении")
+    @NotNull(groups = OnCreate.class, message = "Не должно принимать null значение при создании")
     private Long questionId;
+
+    @NotNull(groups = OnCreate.class, message = "Не должно принимать null значение при создании")
     private String htmlBody;
+
     private LocalDateTime persistDateTime;
     private LocalDateTime dateAcceptTime;
     private Integer countValuable;
     private Boolean isHelpful;
-    @NotNull
+
+    @NotNull(groups = OnUpdate.class, message = "Не должно принимать null значение при обновлении")
+    @NotNull(groups = OnCreate.class, message = "Не должно принимать null значение при создании")
     private UserDto userDto;
 }
