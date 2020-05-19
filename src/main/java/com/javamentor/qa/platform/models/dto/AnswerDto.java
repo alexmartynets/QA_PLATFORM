842c09a683cb21eef0d3dabb67c6250688068b35
oleg <dactiloscopist@mail.ru>
2020-05-19 18:47:55 +0300

    answer_api_toMerge

--- src/main/java/com/javamentor/qa/platform/models/dto/AnswerDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/AnswerDto.java
@@ -2,6 +2,7 @@ package com.javamentor.qa.platform.models.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
 import java.sql.Blob;
 import java.time.LocalDateTime;
 
@@ -13,11 +14,13 @@ import java.time.LocalDateTime;
 @Builder
 public class AnswerDto {
     private Long id;
+    @NotNull
     private Long questionId;
     private String htmlBody;
     private LocalDateTime persistDateTime;
     private LocalDateTime dateAcceptTime;
     private Integer countValuable;
     private Boolean isHelpful;
+    @NotNull
     private UserDto userDto;
 }
