1553e2feb553728e24fc37ff6ab5c276f4b22b41
Alexandr Martynets <amartinets@yandex.ru>
2020-05-19 15:15:20 +0300

    final

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/RoleService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/RoleService.java
@@ -2,5 +2,8 @@ package com.javamentor.qa.platform.service.abstracts.model;
 
 import com.javamentor.qa.platform.models.entity.user.Role;
 
+import java.util.Optional;
+
 public interface RoleService extends ReadWriteService<Role, Long> {
+    Optional<Role> getByRoleName(String roleName);
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/model/RoleServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/RoleServiceImpl.java
@@ -1,19 +1,26 @@
 package com.javamentor.qa.platform.service.impl.model;
 
-import com.javamentor.qa.platform.dao.abstracts.model.RoleDao;
+import com.javamentor.qa.platform.dao.abstracts.model.RoleDAO;
 import com.javamentor.qa.platform.models.entity.user.Role;
 import com.javamentor.qa.platform.service.abstracts.model.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class RoleServiceImpl extends ReadWriteServiceImpl<Role, Long> implements RoleService {
 
-    private final RoleDao roleDao;
+    private final RoleDAO roleDAO;
 
     @Autowired
-    public RoleServiceImpl(RoleDao roleDao) {
-        super(roleDao);
-        this.roleDao = roleDao;
+    public RoleServiceImpl(RoleDAO roleDAO) {
+        super(roleDAO);
+        this.roleDAO = roleDAO;
+    }
+
+    @Override
+    public Optional<Role> getByRoleName(String roleName) {
+        return Optional.empty();
     }
 }
