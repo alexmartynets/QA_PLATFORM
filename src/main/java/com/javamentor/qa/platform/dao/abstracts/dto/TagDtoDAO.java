c78ab85cb1bcca99210a21f9129940ff895ce1ac
oleg <dactiloscopist@mail.ru>
2020-05-20 16:40:09 +0300

    tags_api

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/TagDtoDAO.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/TagDtoDAO.java
@@ -1,5 +1,10 @@
 package com.javamentor.qa.platform.dao.abstracts.dto;
 
+import com.javamentor.qa.platform.models.dto.TagDto;
+
+import java.util.List;
+
 public interface TagDtoDAO {
+    List<TagDto> findAllTagsDtoPagination();
 
 }
