6b6ee74b26d17c3f41f763eec1f626f6d38b9cc6
Alexandr Martynets <amartinets@yandex.ru>
2020-05-21 15:37:21 +0300

    LoggerAndSwagger

--- src/main/java/com/javamentor/qa/platform/models/dto/UserDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/UserDto.java
@@ -2,8 +2,10 @@ package com.javamentor.qa.platform.models.dto;
 
 import com.javamentor.qa.platform.models.util.action.OnCreate;
 import com.javamentor.qa.platform.models.util.action.OnUpdate;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import java.time.LocalDateTime;
@@ -19,35 +21,50 @@ public class UserDto {
 
     @Null(groups = OnCreate.class, message = "Должно принимать null значение при создании")
     @NotNull(groups = OnUpdate.class, message = "Не должно принимать null значение при обновлении")
+    @ApiModelProperty(notes = "Автоматически генерируемый ID пользователя. Не указывать при создании, обязательно указывать при изменении учетной записи")
     private Long id;
 
-    @NotNull
+    @NotNull (groups = OnCreate.class, message = "Поле не должно быть пустым")
+    @ApiModelProperty(notes = "Полное имя пользователя, поле не должно быть пустым")
     private String fullName;
 
     @NotNull
+    @ApiModelProperty(notes = "Email должен быть корректным, смотрите пример")
+    @Email(regexp=".@.\\..*", message = "Email должен быть корректным, смотрите пример")
     private String email;
 
     @NotNull
+    @ApiModelProperty(notes = "Должен содержать минимум 8 символов, 1 заглавную букву и 1 цифру")
     private String password;
 
-    @NotNull
+    @NotNull(groups = OnCreate.class, message = "Автоматически назначается при создании всем пользователям, явно указывать не нужно")
+    @ApiModelProperty(notes = "Автоматически назначается при создании всем пользователям, явно указывать не нужно")
     private String role;
 
+    @ApiModelProperty(notes = "Картинка пользователя, на вход принимает массив байт")
     private byte[] imageUser;
 
+    @ApiModelProperty(notes = "Любая информация о пользователе (не является обязательным полем)")
     private String about;
 
+    @ApiModelProperty(notes = "Город пользователя (не является обязательным полем)")
     private String city;
 
+    @ApiModelProperty(notes = "Ссылка на сайт пользователя")
     private String linkSite;
 
+    @ApiModelProperty(notes = "Ссылка на репозитории GitHub пользователя")
     private String linkGitHub;
 
+    @ApiModelProperty(notes = "Ссылка на страницу VK пользователя")
     private String linkVk;
 
+    @ApiModelProperty(notes = "Репутация пользователя, по умолчанию 0")
     private Integer reputationCount;
 
+    @ApiModelProperty(notes = "Дата создания учетной записи пользователя, явно указывать не нужно, назначается автоматически при создании")
     private LocalDateTime persistDateTime;
 
+    @ApiModelProperty(notes = "Дата изменения учетной записи пользователя, явно указывать не нужно, назначается автоматически при внесении изменений")
     private LocalDateTime lastUpdateDateTime;
 }
--- src/main/java/com/javamentor/qa/platform/webapp/configs/swagger/SwaggerConfig.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/swagger/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.javamentor.qa.platform.webapp.configs.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
 
     @Bean
@@ -19,5 +22,10 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public EmailAnnotationPlugin emailPlugin() {
+        return new EmailAnnotationPlugin();
     }
 }
