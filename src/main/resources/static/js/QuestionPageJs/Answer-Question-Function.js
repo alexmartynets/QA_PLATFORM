957d333c9033497883782dd319e74723c5a80789
AvdeevAnton <avdeev.anthon@yandex.ru>
2020-05-29 15:07:48 +0300

    Beta

--- src/main/resources/static/js/QuestionPageJs/Answer-Question-Function.js
+++ src/main/resources/static/js/QuestionPageJs/Answer-Question-Function.js
@@ -107,7 +107,7 @@ function getTextOfQuestion(id) {
                                                               d="M12 14.002a.998.998 0 01-.998.998H1.001A1 1 0 010 13.999V13c0-2.633 4-4 4-4s.229-.409 0-1c-.841-.62-.944-1.59-1-4 .173-2.413 1.867-3 3-3s2.827.586 3 3c-.056 2.41-.159 3.38-1 4-.229.59 0 1 0 1s4 1.367 4 4v1.002z"></path></svg><h
                                             class=" ml-1 ">${userInfoDto.fullName}</h><h class=" ml-3 " title="уровень репутации">${userInfoDto.reputationCount}</h></div></span></td>
     </tr>`);
-                    if (true === val.isHelpful) {
+                    if (val.isHelpful === true) {
                         document.getElementById("checkMark").innerHTML = "<img src='/images/check-mark.png' width='30' height='30' alt=''>";
                     }
                     $('[data-toggle="popover"]').popover();
@@ -386,7 +386,7 @@ function getSortCurrentTextOfQuestion(id) {
                                                               d="M12 14.002a.998.998 0 01-.998.998H1.001A1 1 0 010 13.999V13c0-2.633 4-4 4-4s.229-.409 0-1c-.841-.62-.944-1.59-1-4 .173-2.413 1.867-3 3-3s2.827.586 3 3c-.056 2.41-.159 3.38-1 4-.229.59 0 1 0 1s4 1.367 4 4v1.002z"></path></svg><h
                                             class=" ml-1 ">${userInfoDto.fullName}</h><h class=" ml-3 " title="уровень репутации">${userInfoDto.reputationCount}</h></div></span></td>
     </tr>`);
-                    if (true === val.isHelpful) {
+                    if (val.isHelpful === true) {
                         document.getElementById("checkMark").innerHTML = "<img src='/images/check-mark.png' width='30' height='30' alt=''>";
                     }
                     $('[data-toggle="popover"]').popover();
@@ -465,7 +465,7 @@ function getSortDateTextOfQuestion(id) {
                                                               d="M12 14.002a.998.998 0 01-.998.998H1.001A1 1 0 010 13.999V13c0-2.633 4-4 4-4s.229-.409 0-1c-.841-.62-.944-1.59-1-4 .173-2.413 1.867-3 3-3s2.827.586 3 3c-.056 2.41-.159 3.38-1 4-.229.59 0 1 0 1s4 1.367 4 4v1.002z"></path></svg><h
                                             class=" ml-1 ">${userInfoDto.fullName}</h><h class=" ml-3 " title="уровень репутации">${userInfoDto.reputationCount}</h></div></span></td>
     </tr>`);
-                    if (true === val.isHelpful) {
+                    if (val.isHelpful === true) {
                         document.getElementById("checkMark").innerHTML = "<img src='/images/check-mark.png' width='30' height='30' alt=''>";
                     }
                     $('[data-toggle="popover"]').popover();
@@ -544,7 +544,7 @@ function getSortReputationTextOfQuestion(id) {
                                                               d="M12 14.002a.998.998 0 01-.998.998H1.001A1 1 0 010 13.999V13c0-2.633 4-4 4-4s.229-.409 0-1c-.841-.62-.944-1.59-1-4 .173-2.413 1.867-3 3-3s2.827.586 3 3c-.056 2.41-.159 3.38-1 4-.229.59 0 1 0 1s4 1.367 4 4v1.002z"></path></svg><h
                                             class=" ml-1 ">${userInfoDto.fullName}</h><h class=" ml-3 " title="уровень репутации">${userInfoDto.reputationCount}</h></div></span></td>
     </tr>`);
-                    if (true === val.isHelpful) {
+                    if (val.isHelpful === true) {
                         document.getElementById("checkMark").innerHTML = "<img src='/images/check-mark.png' width='30' height='30' alt=''>";
                     }
                     $('[data-toggle="popover"]').popover();
