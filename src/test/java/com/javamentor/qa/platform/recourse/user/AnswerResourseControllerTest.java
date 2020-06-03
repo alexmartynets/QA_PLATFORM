5d2ea2946dcc95aeaa6049e1e59c8ce5e576bafd
oleg <dactiloscopist@mail.ru>
2020-06-03 22:08:02 +0300

    answer_api_tests

---
+++ src/test/java/com/javamentor/qa/platform/recourse/user/AnswerResourseControllerTest.java
@@ -0,0 +1,18 @@
+package com.javamentor.qa.platform.recourse.user;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.javamentor.qa.platform.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class AnswerResourseControllerTest extends AbstractIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DataSet(value = {"role.yml", "users.yml"}, cleanBefore = true)
+    void getAllDto()  {
+
+    }
+}
