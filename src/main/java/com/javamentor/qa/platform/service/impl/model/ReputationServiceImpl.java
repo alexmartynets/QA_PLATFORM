d4506f3b3b0ca4727e85fb49ab86027b524c5a56
Artem <torbeev.art@yandex.ru>
2020-06-03 14:50:17 +0300

    add ReputationService

--- src/main/java/com/javamentor/qa/platform/service/impl/model/ReputationServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/ReputationServiceImpl.java
@@ -1,4 +1,4 @@
-package com.javamentor.qa.platform.service.impl;
+package com.javamentor.qa.platform.service.impl.model;
 
 import com.javamentor.qa.platform.dao.abstracts.model.ReputationDAO;
 import com.javamentor.qa.platform.models.entity.Reputation;
