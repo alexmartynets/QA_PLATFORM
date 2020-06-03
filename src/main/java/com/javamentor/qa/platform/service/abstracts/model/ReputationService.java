270dc53826bb975a09f75083ebe212493e3487d2
Artem <torbeev.art@yandex.ru>
2020-06-03 14:49:34 +0300

    add ReputationService

---
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/ReputationService.java
@@ -0,0 +1,6 @@
+package com.javamentor.qa.platform.service.abstracts.model;
+
+import com.javamentor.qa.platform.models.entity.Reputation;
+
+public interface ReputationService extends ReadWriteService<Reputation, Long> {
+}
---
+++ src/main/java/com/javamentor/qa/platform/service/impl/ReputationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.javamentor.qa.platform.service.impl;
+
+import com.javamentor.qa.platform.dao.abstracts.model.ReputationDAO;
+import com.javamentor.qa.platform.models.entity.Reputation;
+import com.javamentor.qa.platform.service.abstracts.model.ReputationService;
+import com.javamentor.qa.platform.service.impl.model.ReadWriteServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReputationServiceImpl extends ReadWriteServiceImpl<Reputation, Long> implements ReputationService {
+
+    private final ReputationDAO reputationDAO;
+
+    @Autowired
+    public ReputationServiceImpl(ReputationDAO reputationDAO) {
+        super(reputationDAO);
+        this.reputationDAO = reputationDAO;
+    }
+}
