d5d775d44353d7a5a6aa3c5ee2d0d8256eb49c52
Artem <torbeev.art@yandex.ru>
2020-06-05 22:33:50 +0300

    добавил EditorDto

---
+++ src/main/java/com/javamentor/qa/platform/models/dto/EditorDto.java
@@ -0,0 +1,36 @@
+package com.javamentor.qa.platform.models.dto;
+
+import com.javamentor.qa.platform.models.util.action.OnCreate;
+import com.javamentor.qa.platform.models.util.action.OnUpdate;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class EditorDto {
+
+    @Null(groups = OnCreate.class, message = "Должно принимать null значение при создании")
+    @NotNull(groups = OnUpdate.class, message = "Не должно принимать null значние при обновлении")
+    private Long editorId;
+
+    private LocalDateTime persistDateTime;
+
+    private Long countChanges;
+
+    private Long id;
+
+    private String fullName;
+
+    private String about;
+
+    private String city;
+
+    private byte[] imageUser;
+}
