91094ed945514d033b6b82457b4f6040a6516d83
Artem <torbeev.art@yandex.ru>
2020-06-05 22:25:59 +0300

    добавил entity Editor

---
+++ src/main/java/com/javamentor/qa/platform/models/entity/Editor.java
@@ -0,0 +1,52 @@
+package com.javamentor.qa.platform.models.entity;
+
+import com.javamentor.qa.platform.models.entity.user.User;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "editor")
+public class Editor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "persist_date", updatable = false)
+    @Type(type = "org.hibernate.type.LocalDateTimeType")
+    private LocalDateTime persistDateTime;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private User user;
+
+    @Column(name = "count_changes")
+    private Integer countChanges;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Editor editor = (Editor) o;
+        return Objects.equals(id, editor.id) &&
+                Objects.equals(persistDateTime, editor.persistDateTime) &&
+                Objects.equals(countChanges, editor.countChanges);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, persistDateTime, countChanges);
+    }
+}
