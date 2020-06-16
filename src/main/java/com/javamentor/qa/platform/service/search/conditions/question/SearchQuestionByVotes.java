0d84d13d808a6af0d07daf07b4bfdcb8f15d1d67
AvdeevAnton <avdeev.anthon@yandex.ru>
2020-06-16 15:46:59 +0300

    1

---
+++ src/main/java/com/javamentor/qa/platform/service/search/conditions/question/SearchQuestionByVotes.java
@@ -0,0 +1,28 @@
+package com.javamentor.qa.platform.service.search.conditions.question;
+
+import com.javamentor.qa.platform.dao.abstracts.dto.SearchQuestionDAO;
+import com.javamentor.qa.platform.models.dto.QuestionDto;
+import com.javamentor.qa.platform.service.search.abstracts.Search;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component("votes")
+public class SearchQuestionByVotes implements Search {
+
+    private final SearchQuestionDAO searchQuestionDAO;
+
+    @Autowired
+    public SearchQuestionByVotes(SearchQuestionDAO searchQuestionDAO) {
+        this.searchQuestionDAO = searchQuestionDAO;
+    }
+
+    @Override
+    public List<QuestionDto> getList(String searchVotes) {
+        if (searchVotes.chars().allMatch(Character::isDigit)) {
+            return searchQuestionDAO.getQuestionsByNumberOfVotes(Integer.parseInt(searchVotes));
+        }
+        return searchQuestionDAO.getQuestionsSortedByVotes();
+    }
+}
