63e7595053924c71a3be75ba86a0420353cf3b9e
oleg <dactiloscopist@mail.ru>
2020-06-18 21:16:20 +0300

    api_answer

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/AnswerService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/AnswerService.java
@@ -2,8 +2,9 @@ package com.javamentor.qa.platform.service.abstracts.model;
 
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 
-import java.util.Map;
-
 public interface AnswerService extends ReadWriteService<Answer, Long>{
-    public void resetIsHelpful(Long questionId);
+    void resetIsHelpful(Long questionId);
+    Answer updateAnswerCount(Long answerId, Boolean count);
+    Answer updateAnswerHelpful(Long answerId, Boolean isHelpful);
+    Answer updateAnswerBody(Long answerId, String htmlBody);
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/model/AnswerServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/AnswerServiceImpl.java
@@ -3,13 +3,19 @@ package com.javamentor.qa.platform.service.impl.model;
 import com.javamentor.qa.platform.dao.abstracts.model.AnswerDAO;
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
 
 @Service
 public class AnswerServiceImpl extends ReadWriteServiceImpl<Answer, Long> implements AnswerService {
 
     private final AnswerDAO answerDAO;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public AnswerServiceImpl(AnswerDAO answerDAO) {
@@ -26,5 +32,53 @@ public class AnswerServiceImpl extends ReadWriteServiceImpl<Answer, Long> implem
             answer.setDateAcceptTime(null);
             answerDAO.update(answer);
         }
+    }
+
+    @Override
+    public Answer updateAnswerCount(Long answerId, Boolean count) {
+        Answer answer = getByKey(answerId);
+        if(answer != null) {
+            if (count) {
+                answer.setCountValuable(answer.getCountValuable() + 1);
+            } else {
+                answer.setCountValuable(answer.getCountValuable() - 1);
+            }
+            update(answer);
+            return answer;
+        }else {
+            logger.error(String.format("Answer with id %s does not exist in DB", answerId));
+            throw new EntityNotFoundException(String.format("Answer with id %s does not exist in DB", answerId));
+        }
+    }
+
+    @Override
+    public Answer updateAnswerHelpful(Long answerId, Boolean isHelpful) {
+        Answer answer = getByKey(answerId);
+        if(answer != null) {
+            resetIsHelpful(answer.getQuestion().getId());
+            answer.setIsHelpful(isHelpful);
+            if(isHelpful){
+                answer.setDateAcceptTime(LocalDateTime.now());
+            }
+            update(answer);
+            return answer;
+        }else {
+            logger.error(String.format("Answer with id %s does not exist in DB", answerId));
+            throw new EntityNotFoundException(String.format("Answer with id %s does not exist in DB", answerId));
+        }
+    }
+
+    @Override
+    public Answer updateAnswerBody(Long answerId, String htmlBody) {
+        Answer answer = getByKey(answerId);
+        if(answer != null) {
+            answer.setHtmlBody(htmlBody);
+            update(answer);
+            return answer;
+        }else {
+            logger.error(String.format("Answer with id %s does not exist in DB", answerId));
+            throw new EntityNotFoundException(String.format("Answer with id %s does not exist in DB", answerId));
+        }
+
     }
 }
