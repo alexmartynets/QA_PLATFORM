255fdbd6430788526435bdaea2e2ad8837f7ae8d
oleg <dactiloscopist@mail.ru>
2020-06-30 01:03:26 +0300

    api_answer test change

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/advice/ApplicationControllerAdvice.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/advice/ApplicationControllerAdvice.java
@@ -2,13 +2,10 @@ package com.javamentor.qa.platform.webapp.controllers.advice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -36,28 +33,6 @@ public class ApplicationControllerAdvice extends ResponseEntityExceptionHandler 
         return handleExceptionInternal(runtimeException, body[body.length - 1].trim(), new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
     }
 
-    @ExceptionHandler(UnsupportedOperationException.class)
-    protected ResponseEntity<Object> illegalArgumentEx(RuntimeException runtimeException, WebRequest webRequest) {
-        logger.info(runtimeException.fillInStackTrace().toString());
-        return handleExceptionInternal(runtimeException, runtimeException.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
-    }
-
-    @ExceptionHandler(InvalidDataAccessApiUsageException.class)
-    protected ResponseEntity<Object> illegalArgumentExe(RuntimeException runtimeException, WebRequest webRequest) {
-        logger.error(runtimeException.getMessage());
-        String[] body = runtimeException.getMessage().split("\\.");
-        return handleExceptionInternal(runtimeException, String.format("%s id can not be null on update.", body[body.length - 1]), new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatus status,
-                                                                  WebRequest request) {
-        FieldError fieldError = ex.getBindingResult().getFieldError();
-        assert fieldError != null;
-        return ResponseEntity.badRequest().body(fieldError.getDefaultMessage());
-    }
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
