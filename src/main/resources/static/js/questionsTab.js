ec23a552352eb71a68aa8c50fe1feefc969ebf95
AvdeevAnton <avdeev.anthon@yandex.ru>
2020-07-04 02:24:04 +0300

    final

--- src/main/resources/static/js/questionsTab.js
+++ src/main/resources/static/js/questionsTab.js
@@ -76,41 +76,114 @@ function getQuestionsSortedByVotes() {
 }
 
 function fillQuestionBlock(q) {
-    let questionData =
-        '<li class="list-group-item">' +
-            '<div class="question-block container mt-4">' +
-                '<div class="voice-block">' +
-                    '<div class="voices voice-item">' +
-                        '<p class="points">' + q.countValuable + '</p>' +
-                        '<span class="smallHeader">Голосов</span>' +
-                    '</div>' +
 
-                    '<div class="answers voice-item">' +
-                        '<p class="points">' + q.countAnswer + '</p>' +
-                        '<span class="smallHeader">Ответов</span>' +
-                    '</div>' +
-                    '<div class="voice-result">' +
-                        '<p><span class="viewCount">' + q.viewCount + ' показов</span></p>' +
-                    '</div>' +
-                '</div>' +
-
-                '<div class="text-block">' +
-                    '<a id="qTitle" href="/question/' + q.id + '"> <h3 class="question-title mt-3">' + q.title + '</h3></a>' +
-                    '<p class="question-body">' + q.description + '</p>' +
-                    '<div class="question-foot">' +
-                        '<div class="tags-block">' +
-                            getTags(q.tags) +
-                        '</div>' +
-                    '</div>' + //question-foot
-                 '</div>'  + //text-block
-                '<div class="user-info">' +
-                    // '<p class="timeOfQuestion">задан <span>' + q.persistDateTime - new Date($.now()) + '</span> минут назад</p>' +
-                    '<p class="user-name">' + q.userDto.fullName + '</p>' +
-                '</div>' +
+    if (q.ignoreTag === true ) {
+        let questionData =
+            '<li class="list-group-item">' +
+            '<div class="question-block container mt-4" style="background-color: #fdf7e3">' +
+            '<div class="voice-block">' +
+            '<div class="voices voice-item">' +
+            '<p class="points">' + q.countValuable + '</p>' +
+            '<span class="smallHeader">Голосов</span>' +
             '</div>' +
-        '</li>'
 
-    return questionData;
+            '<div class="answers voice-item">' +
+            '<p class="points">' + q.countAnswer + '</p>' +
+            '<span class="smallHeader">Ответов</span>' +
+            '</div>' +
+            '<div class="voice-result">' +
+            '<p><span class="viewCount">' + q.viewCount + ' показов</span></p>' +
+            '</div>' +
+            '</div>' +
+
+            '<div class="text-block">' +
+            '<a id="qTitle" href="/question/' + q.id + '"> <h3 class="question-title mt-3">' + q.title + '</h3></a>' +
+            '<p class="question-body">' + q.description + '</p>' +
+            '<div class="question-foot">' +
+            '<div class="tags-block">' +
+            getTags(q.tags) +
+            '</div>' +
+            '</div>' + //question-foot
+            '</div>' + //text-block
+            '<div class="user-info">' +
+            // '<p class="timeOfQuestion">задан <span>' + q.persistDateTime - new Date($.now()) + '</span> минут назад</p>' +
+            '<p class="user-name">' + q.userDto.fullName + '</p>' +
+            '</div>' +
+            '</div>' +
+            '</li>'
+        return questionData;
+    } else  if (q.watchTag === true){
+        let questionData =
+            '<li class="list-group-item">' +
+            '<div class="question-block container mt-4" style="background-color: #fdf7e3">' +
+            '<div class="voice-block">' +
+            '<div class="voices voice-item">' +
+            '<p class="points">' + q.countValuable + '</p>' +
+            '<span class="smallHeader">Голосов</span>' +
+            '</div>' +
+
+            '<div class="answers voice-item">' +
+            '<p class="points">' + q.countAnswer + '</p>' +
+            '<span class="smallHeader">Ответов</span>' +
+            '</div>' +
+            '<div class="voice-result">' +
+            '<p><span class="viewCount">' + q.viewCount + ' показов</span></p>' +
+            '</div>' +
+            '</div>' +
+
+            '<div class="text-block">' +
+            '<a id="qTitle" href="/question/' + q.id + '"> <h3 class="question-title mt-3">' + q.title + '</h3></a>' +
+            '<p class="question-body">' + q.description + '</p>' +
+            '<div class="question-foot">' +
+            '<div class="tags-block">' +
+            getTags(q.tags) +
+            '</div>' +
+            '</div>' + //question-foot
+            '</div>' + //text-block
+            '<div class="user-info">' +
+            // '<p class="timeOfQuestion">задан <span>' + q.persistDateTime - new Date($.now()) + '</span> минут назад</p>' +
+            '<p class="user-name">' + q.userDto.fullName + '</p>' +
+            '</div>' +
+            '</div>' +
+            '</li>'
+        return questionData;
+    } else {
+        let questionData =
+            '<li class="list-group-item">' +
+            '<div class="question-block container mt-4">' +
+            '<div class="voice-block">' +
+            '<div class="voices voice-item">' +
+            '<p class="points">' + q.countValuable + '</p>' +
+            '<span class="smallHeader">Голосов</span>' +
+            '</div>' +
+
+            '<div class="answers voice-item">' +
+            '<p class="points">' + q.countAnswer + '</p>' +
+            '<span class="smallHeader">Ответов</span>' +
+            '</div>' +
+            '<div class="voice-result">' +
+            '<p><span class="viewCount">' + q.viewCount + ' показов</span></p>' +
+            '</div>' +
+            '</div>' +
+
+            '<div class="text-block">' +
+            '<a id="qTitle" href="/question/' + q.id + '"> <h3 class="question-title mt-3">' + q.title + '</h3></a>' +
+            '<p class="question-body">' + q.description + '</p>' +
+            '<div class="question-foot">' +
+            '<div class="tags-block">' +
+            getTags(q.tags) +
+            '</div>' +
+            '</div>' + //question-foot
+            '</div>' + //text-block
+            '<div class="user-info">' +
+            // '<p class="timeOfQuestion">задан <span>' + q.persistDateTime - new Date($.now()) + '</span> минут назад</p>' +
+            '<p class="user-name">' + q.userDto.fullName + '</p>' +
+            '</div>' +
+            '</div>' +
+            '</li>'
+        return questionData;
+    }
+   // return questionData;
 }
 
 function getTags(list) {
