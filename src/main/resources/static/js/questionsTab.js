33b5b3328e5edea9cf833d3da0a475761ddf27d6
AvdeevAnton <avdeev.anthon@yandex.ru>
2020-07-06 18:29:49 +0300

    final

--- src/main/resources/static/js/questionsTab.js
+++ src/main/resources/static/js/questionsTab.js
@@ -9,20 +9,57 @@ $(document).ready(function () {
         let userTagsDto = {};
         userTagsDto.WatchTagName = $('#watch').val();
         $.cookie('WatchTagsCookie', userTagsDto.WatchTagName);
-
+        let watchCookie = userTagsDto.WatchTagName = $('#watch').val();
+        $.cookie(watchCookie+"W", userTagsDto.WatchTagName);
     });
 
     $('#btnIgnore').click(function () {
         let userTagsDto = {};
         userTagsDto.IgnoreTagName = $('#ignore').val();
         $.cookie('IgnoreTagsCookie', userTagsDto.IgnoreTagName);
-
+        let ignoreCookie = userTagsDto.IgnoreTagName = $('#ignore').val();
+        $.cookie(ignoreCookie+"I", userTagsDto.IgnoreTagName);
     });
 })
 
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 function getQuestionsSortedByVotes() {
+    let tableBodyWatchTag = $('#tblWatchTag tbody');
+    tableBodyWatchTag.empty();
+    let tableBodyIgnoreTag = $('#tblIgnoreTag tbody');
+    tableBodyIgnoreTag.empty();
+    let cookies =  document.cookie.split(';');
+    for(let i = 0; i < cookies.length; i++) {
+        let fullCookie = cookies[i];
+        let NameAndTag = fullCookie.split("=");
+        let nameTag = NameAndTag[1];
+        if (NameAndTag[0].endsWith("W")){
+            $.cookie('WatchTagsCookie', nameTag);
+            tableBodyWatchTag.append(`<button type="button" class="btn btn-light btn-sm mr-1" style="background-color: #e1ecf4"><div style="color: #007bff">${nameTag}<span class="badge badge-pill badge-light ml-2" style="background-color: #d1e5f1" title="Удалить метку" onclick="$.removeCookie(${NameAndTag[0]}); window.location.reload();">X</span></div></button><br>`);
+        }
+        if (NameAndTag[0].endsWith("I")){
+            tableBodyIgnoreTag.append(`<button type="button" class="btn btn-light btn-sm mr-1" style="background-color: #e1ecf4"><div style="color: #007bff">${nameTag}<span class="badge badge-pill badge-light ml-2" style="background-color: #d1e5f1" title="Удалить метку" onclick="$.removeCookie(${NameAndTag[0]}); window.location.reload();">X</span></div></button><br>`);
+            $.cookie('IgnoreTagsCookie', nameTag);
+        }
+
+    }
+
+
+
     let watchCookies = $.cookie('WatchTagsCookie');
     let ignoreCookies = $.cookie('IgnoreTagsCookie');
+    if (watchCookies === undefined) {
+        $.cookie('WatchTagsCookie', null);
+    }
+    if (ignoreCookies === undefined) {
+        $.cookie('IgnoreTagsCookie', null);
+    }
     if (watchCookies === undefined && ignoreCookies === undefined) {
         $.ajax({
             url: '/api/user/question/questions',
@@ -35,22 +72,6 @@ function getQuestionsSortedByVotes() {
             }
         })
     } else {
-        if ( watchCookies !== undefined) {
-            let tableBodyWatchTag = $('#tblWatchTag tbody');
-            tableBodyWatchTag.empty();
-            tableBodyWatchTag.append(`<button type="button" class="btn btn-light btn-sm mr-1" style="background-color: #e1ecf4"><div style="color: #007bff">${watchCookies}<span class="badge badge-pill badge-light ml-2" style="background-color: #d1e5f1" title="Удалить метку" onclick="$.removeCookie('WatchTagsCookie'); window.location.reload(); ">X</span></div></button>`);
-        }
-       if ( ignoreCookies !== undefined ){
-        let tableBodyIgnoreTag = $('#tblIgnoreTag tbody');
-        tableBodyIgnoreTag.empty();
-        tableBodyIgnoreTag.append(`<button type="button" class="btn btn-light btn-sm mr-1" style="background-color: #e1ecf4"><div style="color: #007bff">${ignoreCookies}<span class="badge badge-pill badge-light ml-2" style="background-color: #d1e5f1" title="Удалить метку" onclick="$.removeCookie('IgnoreTagsCookie'); window.location.reload();">X</span></div></button>`);
-        }
-        if ( watchCookies === undefined ){
-            $.cookie('WatchTagsCookie', "null");
-        }
-        if ( ignoreCookies === undefined ){
-            $.cookie('IgnoreTagsCookie', "null");
-        }
         $.ajax({
             url: '/api/user/question/watchAndIgnoreTag',
             type: 'POST',
@@ -60,6 +81,17 @@ function getQuestionsSortedByVotes() {
                 'IgnoreTagsName': $.cookie('IgnoreTagsCookie')
             },
             success: function (listOfQuestion) {
+              /*  if (watchCookies  !== undefined && watchCookies !== null) {
+                    let tableBodyWatchTag = $('#tblWatchTag tbody');
+                    tableBodyWatchTag.empty();
+                    tableBodyWatchTag.append(`<button type="button" class="btn btn-light btn-sm mr-1" style="background-color: #e1ecf4"><div style="color: #007bff">${watchCookies}<span class="badge badge-pill badge-light ml-2" style="background-color: #d1e5f1" title="Удалить метку" onclick="$.removeCookie('WatchTagsCookie'); window.location.reload();">X</span></div></button>`);
+                }*/
+              /*  if (ignoreCookies !== undefined && ignoreCookies !== null) {
+                    let tableBodyIgnoreTag = $('#tblIgnoreTag tbody');
+                    tableBodyIgnoreTag.empty();
+                    tableBodyIgnoreTag.append(`<button type="button" class="btn btn-light btn-sm mr-1" style="background-color: #e1ecf4"><div style="color: #007bff">${ignoreCookies}<span class="badge badge-pill badge-light ml-2" style="background-color: #d1e5f1" title="Удалить метку" onclick="$.removeCookie('IgnoreTagsCookie'); window.location.reload();">X</span></div></button>`);
+
+                }*/
                 $.each(listOfQuestion, function (i, q) {
                     $('#getQuestionsQ').append(fillQuestionBlock(q));
                 });
@@ -72,7 +104,6 @@ function fillQuestionBlock(q) {
 
     if (q.ignoreTag === true) {
         let questionData =
-            '<li class="list-group-item">' +
             '<div class="question-block container mt-4" style="background-color: #fdf7e3">' +
             '<div class="voice-block">' +
             '<div class="voices voice-item">' +
@@ -102,12 +133,10 @@ function fillQuestionBlock(q) {
             // '<p class="timeOfQuestion">задан <span>' + q.persistDateTime - new Date($.now()) + '</span> минут назад</p>' +
             '<p class="user-name">' + q.userDto.fullName + '</p>' +
             '</div>' +
-            '</div>' +
-            '</li>'
+            '</div>'
         return questionData;
     } else if (q.watchTag === true) {
         let questionData =
-            '<li class="list-group-item">' +
             '<div class="question-block container mt-4" style="background-color: #fdf7e3">' +
             '<div class="voice-block">' +
             '<div class="voices voice-item">' +
@@ -137,12 +166,10 @@ function fillQuestionBlock(q) {
             // '<p class="timeOfQuestion">задан <span>' + q.persistDateTime - new Date($.now()) + '</span> минут назад</p>' +
             '<p class="user-name">' + q.userDto.fullName + '</p>' +
             '</div>' +
-            '</div>' +
-            '</li>'
+            '</div>'
         return questionData;
     } else if (q.watchTag === true && q.ignoreTag === true) {
         let questionData =
-            '<li class="list-group-item">' +
             '<div class="question-block container mt-4" style="background-color: #fdf7e3">' +
             '<div class="voice-block">' +
             '<div class="voices voice-item">' +
@@ -172,12 +199,10 @@ function fillQuestionBlock(q) {
             // '<p class="timeOfQuestion">задан <span>' + q.persistDateTime - new Date($.now()) + '</span> минут назад</p>' +
             '<p class="user-name">' + q.userDto.fullName + '</p>' +
             '</div>' +
-            '</div>' +
-            '</li>'
+            '</div>'
         return questionData;
     } else {
         let questionData =
-            '<li class="list-group-item">' +
             '<div class="question-block container mt-4">' +
             '<div class="voice-block">' +
             '<div class="voices voice-item">' +
@@ -207,8 +232,7 @@ function fillQuestionBlock(q) {
             // '<p class="timeOfQuestion">задан <span>' + q.persistDateTime - new Date($.now()) + '</span> минут назад</p>' +
             '<p class="user-name">' + q.userDto.fullName + '</p>' +
             '</div>' +
-            '</div>' +
-            '</li>'
+            '</div>'
         return questionData;
     }
 }
